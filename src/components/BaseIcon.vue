0c51e9a357fdc1d35ae3b9ba4a12cd65a92be6c4
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-09-15 00:06:23 +0530

    Global components Registration!

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="svgIcon(name)" />
+    </svg>
+  </div>
+</template>
+
+<script>
+const svgIcon = name => ({ "xlink:href": `/feather-sprite.svg#${name}` });
+
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+  },
+  data() {
+    return {
+      svgIcon,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,34 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+const requireComponent = require.context(
+  "./components",
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split("/")
+        .pop()
+        .replace(/\.\w+$/, "")
+    )
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
