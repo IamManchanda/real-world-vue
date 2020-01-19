3021ba52d49d27366a4cf9f3c800982a1abdab28
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-01-19 14:37:35 +0530

    Add nProgress dependency!

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import upperFirst from "lodash/upperFirst";
 import camelCase from "lodash/camelCase";
+import "nprogress/nprogress.css";
 
 const requireComponent = require.context(
   "./components",
