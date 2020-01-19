ee91da0aa12248163fa94ac153fd2f749d0b6bf5
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-01-19 14:51:09 +0530

    Progress bar interceptors via Axios!

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import NProgress from "nprogress";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
@@ -7,6 +8,16 @@ const api = axios.create({
     Accept: "application/json",
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use(config => {
+  NProgress.start();
+  return config;
+});
+
+api.interceptors.response.use(response => {
+  NProgress.done();
+  return response;
 });
 
 export const getEvents = (perPage, page) =>
